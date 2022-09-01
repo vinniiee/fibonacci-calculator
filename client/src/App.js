2d07a52b545e3989274200843d4b8b34b4032cd2
vinniiee <vs28245@gmail.com>
2022-09-01 12:59:26 +0530

    trigger build

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Fibonacci Calculator</h1>
+        <h1 className="App-title">Fibonacci  Calculator</h1>
         <Link to='/'>Home</Link>
         <Link to='/otherpage'>Other Page</Link>
       </header>
